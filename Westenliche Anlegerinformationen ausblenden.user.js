712c4b07cfdf1bf089c705a7a77077bfedcb91df
JokerGermany <30293477+JokerGermany@users.noreply.github.com>
2021-11-19 12:37:47 +0100

    Wesentliche Anlegerinformationen ausblenden v0.1

---
+++ Westenliche Anlegerinformationen ausblenden.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Westenliche Anlegerinformationen ausblenden
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Westenliche Anlegerinformationen ausblenden
+// @author       JokerGermany
+// @match        https://de.scalable.capital/broker/security?isin=*
+// @icon         https://www.google.com/s2/favicons?domain=scalable.capital
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var Eckdaten = /^Eckdaten$/i;
+
+var divs = document.getElementsByTagName("div");
+
+for (let i = 0;i< divs.length;i++){
+   if (Eckdaten.test(divs[i].innerHTML)) {
+	divs[i].style.display = "none";
+    divs[i+1].style.display = "none";
+    divs[i-1].style.display = "none";
+    divs[i-2].style.display = "none";
+    divs[i-3].style.display = "none";
+   }
+}
+
+})();
